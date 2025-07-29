0ed3e7e5a37383935ea398a88704a43589d366cb
Sonu Maurya <anikitma04@gmail.com>
2025-07-29 22:39:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submit message
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you! Your message has been sent.');
+});
